3ed1955ec164772505b8ffc5bb4d82001c76e5e7
Malcolm Morgan <mem48@users.noreply.github.com>
2018-09-13 16:57:12 +0100

    improting tiplocs

--- R/atoc.R
+++ R/atoc.R
@@ -8,9 +8,17 @@
 #' @param path_out Path to where GTFS files should be saved
 #' @param silent Logical, should progress be shown
 #' @param ncores Numeric, When parallel processing how many cores to use
+#' @param locations character "file" or "package" or path to stops.txt GTFS file, where to get the location data from, default "package"
+#'
+#' @details
+#' Locations
+#' The .msn file contains the physical locations of stations and other TIPLOC codes (e.g. junctions).
+#' However, the quality of the locations is often poor only accurate to about 1km and occasionally very wrong.
+#' Therefore, the UK2GTFS package contains an internal dataset of the TIPLOC locations with better location accuracy.
+#'
 #' @export
 #' }
-atoc2gtfs <- function(path_in,path_out, silent = TRUE, ncores = 1){
+atoc2gtfs <- function(path_in,path_out, silent = TRUE, ncores = 1, locations = "package"){
 
   if(ncores == 1){message(paste0(Sys.time()," This will take some time, make sure you use 'ncores' to enable multi-core processing"))}
   # Is input a zip or a folder
@@ -41,12 +49,10 @@ atoc2gtfs <- function(path_in,path_out, silent = TRUE, ncores = 1){
   msn = importMSN(files[grepl(".msn",files)], silent = silent)
   #ztr = importMCA(files[grepl(".ztr",files)], silent = silent)
 
+
+
   #Construct the GTFS
-  station = msn[[1]]
-  TI = mca[["TI"]]
-  stops.list = station2stops(station = station, TI = TI)
-  stops = stops.list[["stops"]]
-  #stops.lookup = stops.list[["lookup"]]
+
 
   stop_times = mca[["stop_times"]]
   schedule = mca[["schedule"]]
@@ -55,18 +61,29 @@ atoc2gtfs <- function(path_in,path_out, silent = TRUE, ncores = 1){
   names(stop_times) = c("arrival_time","departure_time","stop_id","stop_sequence","Activity","rowID","schedule")
 
 
-  # change stop_times from TIPLOCS to CRS
-  #stop_times = dplyr::left_join(stop_times, stops.lookup, by = c("stop_id" = "TIPLOC"))
-  #stop_times = stop_times[,c("arrival_time","departure_time","match","stop_sequence","Activity","rowID","schedule")]
-  #names(stop_times) = c("arrival_time","departure_time","stop_id","stop_sequence","Activity","rowID","schedule")
-
+  # Get the Station Locations
+  if(locations == "package"){
+    stops = sf::read_sf("./data/tiplocs.geojson", stringsAsFactors = F)
+    #stops = readRDS("data/tiplocs.Rds")
+    stops = cbind(stops, sf::st_coordinates(stops))
+    stops = as.data.frame(stops)
+    stops = stops[,c("stop_id","stop_code", "stop_name","Y","X","valid")]
+    names(stops) = c("stop_id","stop_code", "stop_name","stop_lat","stop_lon","valid")
+    stops$stop_lat = round(stops$stop_lat, 5)
+    stops$stop_lon = round(stops$stop_lon, 5)
+    stops$valid = NULL
+  }else if(locations == "file"){
+    station = msn[[1]]
+    TI = mca[["TI"]]
+    stops.list = station2stops(station = station, TI = TI)
+    stops = stops.list[["stops"]]
+    #stops.lookup = stops.list[["lookup"]]
+  }else{
+    stops = read.csv(locations,stringsAsFactors = F)
+  }
 
   # remove any unused stops
   stops = stops[stops$stop_id %in% stop_times$stop_id,]
-
-  # remove any stop_times at unknown stops
-  # these are junctions that have scheduels arrive and departute times
-  #stop_times = stop_times[stop_times$stop_id %in% stops$stop_id,]
 
 
   timetables = schedule2routes_alt(stop_times = stop_times, schedule = schedule, silent = silent, ncores = ncores)
@@ -76,6 +93,7 @@ atoc2gtfs <- function(path_in,path_out, silent = TRUE, ncores = 1){
   routes = timetables[["routes"]]
   stop_times = timetables[["stop_times"]]
   trips = timetables[["trips"]]
+  agency = read.csv("data/agency.txt", stringsAsFactors = F)
 
   write.csv(calendar,paste0(path_out,"/calendar.txt"), row.names = FALSE )
   write.csv(calendar_dates,paste0(path_out,"/calendar_dates.txt"), row.names = FALSE )
@@ -83,7 +101,7 @@ atoc2gtfs <- function(path_in,path_out, silent = TRUE, ncores = 1){
   write.csv(stop_times,paste0(path_out,"/stop_times.txt"), row.names = FALSE )
   write.csv(trips,paste0(path_out,"/trips.txt"), row.names = FALSE )
   write.csv(stops,paste0(path_out,"/stops.txt"), row.names = FALSE )
-
+  write.csv(agency,paste0(path_out,"/agency.txt"), row.names = FALSE )
 }
 
 
--- RScripts/prep_naptan.R
+++ RScripts/prep_naptan.R
@@ -1,30 +1,82 @@
 # clean naptan for gtfs
 library(sf)
 library(dplyr)
+library(tmap)
+tmap_mode("view")
 
 naptan = read.csv("D:/Users/earmmor/OneDrive - University of Leeds/Routing/Naptan/NaPTANcsv/Stops.csv")
 naptan = naptan[,c("ATCOCode","NaptanCode","CommonName","Latitude","Longitude","StopType")]
 
-corpus <- jsonlite::fromJSON("D:/Users/earmmor/OneDrive - University of Leeds/NetworkRail/Reference Data/CORPUSExtract.json/corpus-out.json")
-corpus <- corpus[[1]]
-corpus$TIPLOC[corpus$TIPLOC == ""] <- NA
-corpus$TIPLOC[corpus$TIPLOC == " "] <- NA
-
-foo <- dplyr::left_join(naptan, corpus, by = c("ATCOCode" = "NLC"))
-
-library(osmdata)
-bb = getbb("Great Britain")
-#q <- opq(bbox = c(50, -11, 61, 2)) %>%
-q <- opq(bbox = c(50, -1, 52, 2)) %>%
-  add_osm_feature(key = 'ref:tiploc')
-res = osmdata_sf(q = q)
+# corpus <- jsonlite::fromJSON("D:/Users/earmmor/OneDrive - University of Leeds/NetworkRail/Reference Data/CORPUSExtract.json/corpus-out.json")
+# corpus <- corpus[[1]]
+# corpus$TIPLOC[corpus$TIPLOC == ""] <- NA
+# corpus$TIPLOC[corpus$TIPLOC == " "] <- NA
+#
+# foo <- dplyr::left_join(naptan, corpus, by = c("ATCOCode" = "NLC"))
+#
+# library(osmdata)
+# bb = getbb("Great Britain")
+# #q <- opq(bbox = c(50, -11, 61, 2)) %>%
+# q <- opq(bbox = c(50, -1, 52, 2)) %>%
+#   add_osm_feature(key = 'ref:tiploc')
+# res = osmdata_sf(q = q)
 
 railrefs = read.csv("D:/Users/earmmor/OneDrive - University of Leeds/Routing/Naptan/NaPTANcsv/RailReferences.csv", stringsAsFactors = F)
 railrefs = railrefs[,c("AtcoCode","TiplocCode","CrsCode","StationName","Easting","Northing")]
 railrefs = st_as_sf(railrefs, coords = c("Easting","Northing"), crs = 27700)
 railrefs = st_transform(railrefs, 4326)
-coords = as.data.frame(st_coordinates(railrefs))
-names(coords) = c("stop_lat","stop_lon")
+
+
+
+# coords = as.data.frame(st_coordinates(railrefs))
+# names(coords) = c("stop_lat","stop_lon")
 railrefs = as.data.frame(railrefs)
-railrefs$geometry = NULL
-railrefs = cbind(railrefs,coords)
+# railrefs$geometry = NULL
+# railrefs = cbind(railrefs,coords)
+railrefs = railrefs[,c("TiplocCode","StationName","geometry")]
+
+tiplocs = st_read("data/tiplocs.geojson", stringsAsFactors = F)
+tiplocs = as.data.frame(tiplocs)
+
+join = left_join(tiplocs, railrefs, by = c("stop_id" = "TiplocCode"))
+join$geometry = ifelse(lengths(join$geometry.y) == 2, join$geometry.y, join$geometry.x)
+join$stop_name = ifelse(is.na(join$StationName), join$stop_name, join$StationName)
+join$valid = ifelse(is.na(join$StationName), 0,1)
+
+join = join[,c("stop_id","stop_code","stop_name","valid","geometry")]
+join = st_sf(join, crs = 4326)
+
+st_write(join, "data/tiplocs.geojson", delete_dsn = T)
+
+qtm(tiplocs, dots.col = "red") +
+  qtm(railrefs)
+
+
+###################
+
+tiplocs = st_read("data/tiplocs.geojson")
+railrefs_sub = railrefs[!railrefs$TiplocCode %in% tiplocs$stop_id,]
+names(railrefs_sub) = c("AtcoCode","stop_id","stop_code","stop_name", "geometry")
+railrefs_sub$valid = 1
+railrefs_sub = railrefs_sub[,c("stop_id","stop_code","stop_name","valid","geometry")]
+
+tiplocs2 = rbind(tiplocs,railrefs_sub)
+
+st_write(tiplocs2, "data/tiplocs.geojson", delete_dsn = T)
+
+
+#######
+tiplocs = st_read("data/tiplocs.geojson")
+
+open_tiplocs = read.csv("data/open_tiplocs.csv", stringsAsFactors = F)
+open_tiplocs = open_tiplocs[!open_tiplocs$TIPLOC %in% tiplocs2$stop_id,]
+open_tiplocs = st_as_sf(open_tiplocs, coords = c("EASTING", "NORTHING"), crs = 27700)
+names(open_tiplocs) = c("stop_id","stop_name","geometry")
+open_tiplocs$stop_code = NA
+open_tiplocs$valid = NA
+open_tiplocs = open_tiplocs[,c("stop_id","stop_code","stop_name","valid","geometry")]
+open_tiplocs = st_transform(open_tiplocs, 4326)
+
+tiplocs3 = rbind(tiplocs,open_tiplocs)
+
+st_write(tiplocs3, "data/tiplocs.geojson", delete_dsn = T)
