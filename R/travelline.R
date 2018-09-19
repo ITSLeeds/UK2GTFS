9506f683f2dd7260f7b810b95e4a9becfe01647c
Malcolm Morgan <mem48@users.noreply.github.com>
2018-09-19 23:21:34 +0100

    more

--- R/travelline.R
+++ R/travelline.R
@@ -13,11 +13,19 @@ Routes <- data.frame(matrix(unlist(Routes), nrow=length(Routes), byrow=T),string
 
 
 JourneyPatternSections = xml_data["JourneyPatternSections"]
+JourneyPatternSections = JourneyPatternSections[[1]]
+foo = JourneyPatternSections[[1]]
+
 Operators = xml_data["Operators"]
 Operators = Operators[[1]]
 Operators <- data.frame(matrix(unlist(Operators), nrow=length(Operators), byrow=T),stringsAsFactors=FALSE)
 
 
 Services = xml_data["Services"]
+Services = Services[[1]][[1]]
+#Services <- data.frame(matrix(unlist(Services), nrow=length(Services), byrow=T),stringsAsFactors=FALSE)
+
+
+
 VehicleJourneys = xml_data["VehicleJourneys"]
 #attrs = xml_data[".attrs"]
