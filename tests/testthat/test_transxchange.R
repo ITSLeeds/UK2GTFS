631f22980656d7452b0959421d25ca34ee09c490
Malcolm Morgan <mem48@users.noreply.github.com>
2020-04-12 18:41:25 +0100

    basic test

--- tests/testthat/test_transxchange.R
+++ tests/testthat/test_transxchange.R
@@ -5,11 +5,11 @@ context("Test the TransXChange Functions")
 context("Get the example files")
 file_path <- file.path(tempdir(),"gtfs_tests")
 dir.create(file_path)
-unzip("inst/extdata/transxchange.zip", exdir = file_path)
+#unzip("inst/extdata/transxchange.zip", exdir = file_path)
 
 test_that("test transxchange2gtfs", {
-  transxchange2gtfs(path_in = "inst/extdata/transxchange.zip",
+  transxchange2gtfs(path_in = "extdata/transxchange.zip",
                     path_out = file_path)
-  expect_true(file.exists(file.path(file_path,"/gtfs.zip")))
+  expect_true(file.exists(file.path(file_path,"gtfs.zip")))
 
 })
