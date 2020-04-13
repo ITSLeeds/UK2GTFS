a56e13bc7af85f7c246fbb1407c6186d0b4a9e27
Malcolm Morgan <mem48@users.noreply.github.com>
2020-04-13 15:16:41 +0100

    update tests

--- tests/testthat/test_transxchange.R
+++ tests/testthat/test_transxchange.R
@@ -1,8 +1,5 @@
 # This tests will run without OTP setup.
-context("Test the TransXChange Functions")
-
-
-context("Get the example files")
+context("Get the example transXchange files")
 file_path <- file.path(tempdir(),"gtfs_tests")
 dir.create(file_path)
 #unzip("inst/extdata/transxchange.zip", exdir = file_path)
@@ -14,10 +11,18 @@ test_that("test transxchange data is there", {
 
 
 
-test_that("test transxchange2gtfs", {
+test_that("test transxchange2gtfs multicore", {
   transxchange2gtfs(path_in = file.path(.libPaths()[1],"UK2GTFS/extdata/transxchange.zip"),
                     path_out = file_path,
                     ncores = 2)
+  expect_true(file.exists(file.path(file_path,"gtfs.zip")))
+
+})
+
+test_that("test transxchange2gtfs singlecore", {
+  transxchange2gtfs(path_in = file.path(.libPaths()[1],"UK2GTFS/extdata/transxchange.zip"),
+                    path_out = file_path,
+                    ncores = 1)
   expect_true(file.exists(file.path(file_path,"gtfs.zip")))
 
 })
