e4fa12beb2e54a20356c538e79415ff39ee14d25
Malcolm Morgan <mem48@users.noreply.github.com>
2020-04-14 20:53:53 +0100

    improve code coverage

--- tests/testthat/test_atoc.R
+++ tests/testthat/test_atoc.R
@@ -8,8 +8,7 @@ test_that("test atoc data is there", {
 
 })
 
-
-
+context("Test the main atoc function")
 
 test_that("test atoc2gtfs singlecore", {
   gtfs <- atoc2gtfs(path_in = file.path(.libPaths()[1],"UK2GTFS/extdata/atoc.zip"),
@@ -20,7 +19,43 @@ test_that("test atoc2gtfs singlecore", {
   expect_true(length(gtfs) == 7)
 })
 
+context("Test import functions")
+
+unzip(file.path(.libPaths()[1],"UK2GTFS/extdata/atoc.zip"), exdir = file_path)
+
+# files used
+# "example.mca"  "ttisf585.alf" "ttisf585.flf" "ttisf585.msn"
+
+# files ignored
+# "ttisf585.ztr" "ttisf585.dat" "ttisf585.set" "ttisf585.tsi"
 
 
+test_that("test importALF", {
+  r1 <- importALF(file.path(file_path,"ttisf585.alf"))
+  expect_true(class(r1) == "data.frame")
+})
+
+test_that("test importFLF", {
+  r1 <- importFLF(file.path(file_path,"ttisf585.flf"))
+  expect_true(class(r1) == "data.frame")
+})
+
+test_that("test importMSN", {
+  r1 <- importMSN(file.path(file_path,"ttisf585.msn"))
+  expect_true(class(r1) == "data.frame")
+})
+
+test_that("test importFLF", {
+  r1 <- importFLF(file.path(file_path,"ttisf585.flf"))
+  expect_true(class(r1) == "data.frame")
+})
 
 
+test_that("test importMCA", {
+  r1 <- importMCA(file.path(file_path,"example.mca"),
+                  full_import = TRUE,
+                  silent = FALSE)
+  expect_true(class(r1) == "list")
+})
+
+unlink(file_path, recursive = TRUE)
