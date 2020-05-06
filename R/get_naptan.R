0ae4a33abcdc7ba9bbfdbe8ec3bb2e637319154a
Malcolm Morgan <mem48@users.noreply.github.com>
2020-05-06 10:34:34 +0100

    Revert to csv version od naptan
    some stops are missing from the gtfs version

--- R/get_naptan.R
+++ R/get_naptan.R
@@ -2,7 +2,7 @@
 #'
 #' Download the NaPTAN stop locations for more information on NaPTAN see
 #' https://data.gov.uk/dataset/ff93ffc1-6656-47d8-9155-85ea0b8f2251/national-public-transport-access-nodes-naptan
-#' @param url character, url to the GTFS format NaPTAN
+#' @param url character, url to the csv format NaPTAN
 #' @param naptan_extra data frame of missing stops
 #' @return data frame of stop locations
 #' @details TransXchange does not store the location of bus stops, so this
@@ -10,47 +10,47 @@
 #'   missing bus stops which are added by UK2GTFS.
 #'
 #' @export
-get_naptan <- function(url = "http://naptan.app.dft.gov.uk/datarequest/GTFS.ashx", naptan_extra = naptan_missing) {
-  utils::download.file(url = url, destfile = "naptan.zip", mode = "wb")
+
+get_naptan <- function(url = "http://naptan.app.dft.gov.uk/DataRequest/Naptan.ashx?format=csv", naptan_extra = naptan_missing) {
+  utils::download.file(url = url, destfile = "naptan.zip", mode = "wb", quiet = TRUE)
   dir.create("temp_naptan")
-  utils::unzip("naptan.zip", exdir = "temp_naptan")
-  naptan <- utils::read.csv("temp_naptan/Stops.txt", stringsAsFactors = FALSE, sep = "\t")
-  unlink("temp_naptan", recursive = TRUE)
+  utils::unzip("naptan.zip", exdir = "temp_naptan", files = "Stops.csv")
+  naptan <- readr::read_csv("temp_naptan/Stops.csv", progress = FALSE)
+  unlink("temp_naptan", recursive = T)
   file.remove("naptan.zip")
 
   # clean file
-  names(naptan) <- c("stop_id", "stop_code", "stop_name", "stop_lat", "stop_lon", "stop_url", "vehicle_type")
-  naptan <- naptan[, names(naptan_extra)]
+  naptan <- naptan[, c("ATCOCode", "NaptanCode", "CommonName", "Longitude", "Latitude")]
+  names(naptan) <- c("stop_id", "stop_code", "stop_name", "stop_lon", "stop_lat")
 
-  # format
   naptan$stop_lon <- format(round(naptan$stop_lon, 6), scientific = FALSE)
   naptan$stop_lat <- format(round(naptan$stop_lat, 6), scientific = FALSE)
 
-  # Append extra data
-  naptan_extra <- naptan_extra[!naptan_extra$stop_id %in% naptan$stop_id, ]
+  # Append alterative tags
+  naptan_missing <- naptan_missing[!naptan_missing$stop_id %in% naptan$stop_id,]
   naptan <- rbind(naptan, naptan_extra)
 
   return(naptan)
 }
 
-#
-# get_naptan <- function(url = "http://naptan.app.dft.gov.uk/DataRequest/Naptan.ashx?format=csv", naptan_extra = naptan_missing) {
+# get_naptan <- function(url = "http://naptan.app.dft.gov.uk/datarequest/GTFS.ashx", naptan_extra = naptan_missing) {
 #   utils::download.file(url = url, destfile = "naptan.zip", mode = "wb")
-#   dir.create("temp")
-#   utils::unzip("naptan.zip", exdir = "temp")
-#   naptan <- utils::read.csv("temp/stops.csv", stringsAsFactors = F)
-#   unlink("temp", recursive = T)
+#   dir.create("temp_naptan")
+#   utils::unzip("naptan.zip", exdir = "temp_naptan")
+#   naptan <- utils::read.csv("temp_naptan/Stops.txt", stringsAsFactors = FALSE, sep = "\t")
+#   unlink("temp_naptan", recursive = TRUE)
 #   file.remove("naptan.zip")
 #
 #   # clean file
-#   naptan <- naptan[, c("ATCOCode", "NaptanCode", "CommonName", "Longitude", "Latitude")]
-#   names(naptan) <- c("stop_id", "stop_code", "stop_name", "stop_lon", "stop_lat")
+#   names(naptan) <- c("stop_id", "stop_code", "stop_name", "stop_lat", "stop_lon", "stop_url", "vehicle_type")
+#   naptan <- naptan[, names(naptan_extra)]
 #
+#   # format
 #   naptan$stop_lon <- format(round(naptan$stop_lon, 6), scientific = FALSE)
 #   naptan$stop_lat <- format(round(naptan$stop_lat, 6), scientific = FALSE)
 #
-#   # Append alterative tags
-#   naptan_missing <- naptan_missing[!naptan_missing$stop_id %in% naptan$stop_id,]
+#   # Append extra data
+#   naptan_extra <- naptan_extra[!naptan_extra$stop_id %in% naptan$stop_id, ]
 #   naptan <- rbind(naptan, naptan_extra)
 #
 #   return(naptan)
