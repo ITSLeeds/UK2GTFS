b00a2dbffd92b2900bce258574ca9488fd0cf1b9
Malcolm Morgan <mem48@users.noreply.github.com>
2021-10-27 19:22:47 +0100

    fix the locked file problem

--- R/gtfs_read.R
+++ R/gtfs_read.R
@@ -9,7 +9,7 @@
 gtfs_read <- function(path){
   checkmate::assert_file_exists(path)
 
-  tmp_folder <- file.path(getwd(),"gtfsread")
+  tmp_folder <- file.path(tempdir(),"gtfsread")
   dir.create(tmp_folder)
   utils::unzip(path, exdir = tmp_folder)
 
@@ -19,20 +19,21 @@ gtfs_read <- function(path){
   message_log <- c("Unable to find optional files: ")
 
   if(checkmate::test_file_exists(file.path(tmp_folder,"agency.txt"))){
-    gtfs$agency <- readr::read_csv(file.path(tmp_folder,"agency.txt"))
+    gtfs$agency <- readr::read_csv(file.path(tmp_folder,"agency.txt"), lazy = FALSE)
   } else {
     warning("Unable to find required file: agency.txt")
   }
 
   if(checkmate::test_file_exists(file.path(tmp_folder,"stops.txt"))){
-    gtfs$stops <- readr::read_csv(file.path(tmp_folder,"stops.txt"))
+    gtfs$stops <- readr::read_csv(file.path(tmp_folder,"stops.txt"), lazy = FALSE)
   } else {
     warning("Unable to find required file: stops.txt")
   }
 
   if(checkmate::test_file_exists(file.path(tmp_folder,"routes.txt"))){
     gtfs$routes <- readr::read_csv(file.path(tmp_folder,"routes.txt"),
-                                   col_types = readr::cols(route_id = readr::col_character()))
+                                   col_types = readr::cols(route_id = readr::col_character()),
+                                   lazy = FALSE)
   } else {
     warning("Unable to find required file: routes.txt")
   }
@@ -40,7 +41,8 @@ gtfs_read <- function(path){
   if(checkmate::test_file_exists(file.path(tmp_folder,"trips.txt"))){
     gtfs$trips <- readr::read_csv(file.path(tmp_folder,"trips.txt"),
                                   col_types = readr::cols(trip_id = readr::col_character(),
-                                                          route_id = readr::col_character()))
+                                                          route_id = readr::col_character()),
+                                  lazy = FALSE)
   } else {
     warning("Unable to find required file: trips.txt")
   }
@@ -49,7 +51,8 @@ gtfs_read <- function(path){
     gtfs$stop_times <- readr::read_csv(file.path(tmp_folder,"stop_times.txt"),
                                        col_types = readr::cols(trip_id = readr::col_character(),
                                                                departure_time = readr::col_character(),
-                                                               arrival_time = readr::col_character()))
+                                                               arrival_time = readr::col_character()),
+                                       lazy = FALSE)
     gtfs$stop_times$arrival_time <- lubridate::hms(gtfs$stop_times$arrival_time)
     gtfs$stop_times$departure_time <- lubridate::hms(gtfs$stop_times$departure_time)
 
@@ -60,7 +63,8 @@ gtfs_read <- function(path){
   if(checkmate::test_file_exists(file.path(tmp_folder,"calendar.txt"))){
     gtfs$calendar <- readr::read_csv(file.path(tmp_folder,"calendar.txt"),
                                      col_types = readr::cols(start_date = readr::col_date(format = "%Y%m%d"),
-                                                             end_date = readr::col_date(format = "%Y%m%d")))
+                                                             end_date = readr::col_date(format = "%Y%m%d")),
+                                     lazy = FALSE)
 
   } else {
     message("Unable to find conditionally required file: calendar.txt")
@@ -68,38 +72,42 @@ gtfs_read <- function(path){
 
   if(checkmate::test_file_exists(file.path(tmp_folder,"calendar_dates.txt"))){
     gtfs$calendar_dates <- readr::read_csv(file.path(tmp_folder,"calendar_dates.txt"),
-                                           col_types = readr::cols(date = readr::col_date(format = "%Y%m%d")))
+                                           col_types = readr::cols(date = readr::col_date(format = "%Y%m%d")),
+                                           lazy = FALSE)
   } else {
     message("Unable to find conditionally required file: calendar_dates.txt")
   }
 
   if(checkmate::test_file_exists(file.path(tmp_folder,"fare_attributes.txt"))){
-    gtfs$fare_attributes <- readr::read_csv(file.path(tmp_folder,"fare_attributes.txt"))
+    gtfs$fare_attributes <- readr::read_csv(file.path(tmp_folder,"fare_attributes.txt"),
+                                            lazy = FALSE)
   } else {
     message_log <- c(message_log, "fare_attributes.txt")
   }
 
   if(checkmate::test_file_exists(file.path(tmp_folder,"fare_rules.txt"))){
-    gtfs$fare_rules <- readr::read_csv(file.path(tmp_folder,"fare_rules.txt"))
+    gtfs$fare_rules <- readr::read_csv(file.path(tmp_folder,"fare_rules.txt"),
+                                       lazy = FALSE)
   } else {
     message_log <- c(message_log, "fare_rules.txt")
   }
 
   if(checkmate::test_file_exists(file.path(tmp_folder,"shapes.txt"))){
-    gtfs$shapes <- readr::read_csv(file.path(tmp_folder,"shapes.txt"))
+    gtfs$shapes <- readr::read_csv(file.path(tmp_folder,"shapes.txt"),
+                                   lazy = FALSE)
   } else {
     message_log <- c(message_log, "shapes.txt")
   }
 
   if(checkmate::test_file_exists(file.path(tmp_folder,"transfers.txt"))){
-    gtfs$transfers <- readr::read_csv(file.path(tmp_folder,"transfers.txt"))
+    gtfs$transfers <- readr::read_csv(file.path(tmp_folder,"transfers.txt"),
+                                      lazy = FALSE)
   } else {
     message_log <- c(message_log, "transfers.txt")
   }
 
+  unlink(tmp_folder, recursive = TRUE)
 
-  unlink(file.path(tmp_folder,files))
-  unlink(tmp_folder, recursive = TRUE, force = TRUE)
 
   if(length(message_log) > 0){
     message(paste(message_log, collapse = " "))
